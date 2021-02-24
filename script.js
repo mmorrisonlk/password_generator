5b0f49e200c60ff13685745a28c6d467dd32ae20
mmorrisonlk <mmorrisonlk@gmail.com>
2021-02-23 21:11:43 -0600

    Commented out later methods to focus on getting a working prompt with built in sanity checks

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    var passwordLength = parseInt(window.prompt("Enter a number between 8 and 128 to determine password length:"), 10);
+
+    if (Number.isInteger(passwordLength) == false) {
+        window.alert("Please enter an integer value for Password Length")
+        return;
+    }
+
+    if (passwordLength<8 || passwordLength>128) {
+        window.alert("Invalid Password Length")
+        return;
+    }
+    console.log(passwordLength);
+
+    // var password = generatePassword();
+    // var passwordText = document.querySelector("#password");
+
+    // passwordText.value = password;
 
 }
 
